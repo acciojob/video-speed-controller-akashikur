f31f2a0fd9578edfdd5d00cd5a2acaa5805344f2
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-27 22:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,19 @@
-  const volumeInput = document.querySelector('.player__slider[name="volume"]');
-  const playbackSpeedInput = document.querySelector('.player__slider[name="playbackRate"]');
-  const playButton = document.querySelector('.player__button');
-  const skipButtons = document.querySelectorAll('[data-skip]');
+const video = document.querySelector('.flex');
+const speedBar = document.querySelector('.speed-bar');
+let isMouseDown = false;
 
-  // Function to toggle play/pause
-  function togglePlay() {
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  }
-
-  // Function to update play/pause button icon
-  function updatePlayButtonIcon() {
-    playButton.textContent = video.paused ? '►' : '❚ ❚';
-  }
-
-  // Function to update playback speed
-  function updatePlaybackSpeed() {
-    video.playbackRate = parseFloat(playbackSpeedInput.value);
-    speedBar.textContent = playbackSpeedInput.value + '×';
-  }
-
-  // Function to handle skip buttons
-  function handleSkip() {
-    const skipTime = parseFloat(this.dataset.skip);
-    video.currentTime += skipTime;
-  }
-
-  // Event listeners
-  video.addEventListener('click', togglePlay);
-  video.addEventListener('play', updatePlayButtonIcon);
-  video.addEventListener('pause', updatePlayButtonIcon);
-  video.addEventListener('timeupdate', updateProgress);
-
-  playButton.addEventListener('click', togglePlay);
-
-  volumeInput.addEventListener('input', handleVolumeChange);
-  playbackSpeedInput.addEventListener('input', updatePlaybackSpeed);
-
-  skipButtons.forEach(button => button.addEventListener('click', handleSkip));
-});
-
-// Function to update progress bar
-function updateProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressFilled.style.flexBasis = `${percent}%`;
+function handleSpeedScrub(e) {
+  if (!isMouseDown) return;
+  const scrubY = e.pageY - this.offsetTop;
+  const percent = scrubY / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const playbackRate = percent * (max - min) + min;
+  speedBar.textContent = `${playbackRate.toFixed(1)}x`;
+  video.playbackRate = playbackRate;
 }
 
-// Function to handle volume change
-function handleVolumeChange() {
-  video.volume = volumeInput.value;
-}
+speedBar.addEventListener('mousedown', () => isMouseDown = true);
+speedBar.addEventListener('mouseup', () => isMouseDown = false);
+speedBar.addEventListener('mouseleave', () => isMouseDown = false);
+speedBar.addEventListener('mousemove', handleSpeedScrub);
